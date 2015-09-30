97cf654459719dd11506a218d5f6c7ca8ca72395
Connor Braa <cbraa@pivotal.io>
2015-09-30 15:30:51 -0700

    Add ReporterInfo to lrp_convergence_gather_test
    [#101575396]
    
    Signed-off-by: Kris Hicks <khicks@pivotal.io>

--- lrp_convergence_gather_test.go
+++ lrp_convergence_gather_test.go
@@ -1,22 +1,27 @@
 package benchmark_bbs_test
 
 import (
+	"github.com/cloudfoundry-incubator/benchmark-bbs/reporter"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+)
+
+const (
+	ConvergenceGathering = "ConvergenceGathering"
 )
 
 var _ = Describe("Gathering", func() {
 	Measure("data for convergence", func(b Benchmarker) {
 		guids := map[string]struct{}{}
 
-		b.Time("BBS' internal gathering of Actual LRPs", func() {
+		b.Time("BBS' internal gathering of LRPs", func() {
 			_, err := etcdDB.GatherActualLRPs(logger, guids)
 			Expect(err).NotTo(HaveOccurred())
-		})
 
-		b.Time("BBS' internal gathering of Desired LRPs", func() {
-			_, err := etcdDB.GatherDesiredLRPs(logger, guids)
+			_, err = etcdDB.GatherDesiredLRPs(logger, guids)
 			Expect(err).NotTo(HaveOccurred())
+		}, reporter.ReporterInfo{
+			MetricName: ConvergenceGathering,
 		})
 	}, 10)
 })
