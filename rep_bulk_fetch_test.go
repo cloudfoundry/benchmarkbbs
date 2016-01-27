95c8631d6d39010a41ae27848c53451bbdce1e57
Andrew Edgar <aedgar@ca.ibm.com>
2016-01-27 10:01:27 -0800

    Enable rep bulk loop test.
    * Randomly sleep before each bulk loop queries the bbs
    * from 0-repBulkCycle.
    
    [#111842773]
    
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>

--- rep_bulk_fetch_test.go
+++ rep_bulk_fetch_test.go
@@ -2,6 +2,7 @@ package benchmark_bbs_test
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -15,12 +16,19 @@ const (
 	RepBulkFetching = "RepBulkFetching"
 )
 
-var repBulkCycle = 30 * time.Second
+var repBulkCycle = 30
 
 var BenchmarkRepFetching = func(numReps, numTrials int) {
-	PDescribe("Fetching for rep bulk loop", func() {
+	Describe("Fetching for rep bulk loop", func() {
+		var (
+			r *rand.Rand
+		)
+
+		BeforeEach(func() {
+			r = rand.New(rand.NewSource(99))
+		})
+
 		Measure("data for rep bulk", func(b Benchmarker) {
-			time.Sleep(repBulkCycle)
 			b.Time("rep bulk loop", func() {
 				wg := sync.WaitGroup{}
 				for i := 0; i < numReps; i++ {
@@ -30,6 +38,12 @@ var BenchmarkRepFetching = func(numReps, numTrials int) {
 						defer func() {
 							wg.Done()
 						}()
+
+						// We are setting the sleep duration to a constant spread of randomly generated numbers.
+						// This ensures that there should be no issues with timing causing a flacky test as long as
+						// we are confident that the numbers should pass.
+						sleepDuration := time.Duration(repBulkCycle-r.Intn(repBulkCycle)) * time.Second
+						time.Sleep(sleepDuration)
 
 						defer GinkgoRecover()
 						b.Time(fmt.Sprintf("%s: fetch actualLRPs", cellID), func() {
