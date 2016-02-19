cf1e0a71b9c1864875425fab308663cc4ed13216
Amelia Downs <adowns@pivotal.io>
2016-02-19 10:12:54 -0800

    Add claiming and starting to simulated rep bulk loop
    [#113854839]
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- rep_bulk_fetch_test.go
+++ rep_bulk_fetch_test.go
@@ -13,8 +13,10 @@ import (
 )
 
 const (
-	RepBulkFetching = "RepBulkFetching"
-	RepBulkLoop     = "RepBulkLoop"
+	RepBulkFetching   = "RepBulkFetching"
+	RepBulkLoop       = "RepBulkLoop"
+	RepClaimActualLRP = "RepClaimActualLRP"
+	RepStartActualLRP = "RepStartActualLRP"
 )
 
 var repBulkCycle = 30
@@ -22,22 +24,22 @@ var repBulkCycle = 30
 var BenchmarkRepFetching = func(numReps, numTrials int) {
 	Describe("Fetching for rep bulk loop", func() {
 		Measure("data for rep bulk", func(b Benchmarker) {
-			b.Time("rep bulk loop", func() {
-				wg := sync.WaitGroup{}
-				for i := 0; i < numReps; i++ {
-					cellID := fmt.Sprintf("cell-%d", i)
-					wg.Add(1)
-					go func(cellID string) {
-						defer wg.Done()
+			wg := sync.WaitGroup{}
+			for i := 0; i < numReps; i++ {
+				cellID := fmt.Sprintf("cell-%d", i)
+				wg.Add(1)
+				go func(cellID string) {
+					defer wg.Done()
 
-						for j := 0; j < numTrials; j++ {
-							sleepDuration := 30 * time.Second
-							if j == 0 {
-								numMilli := rand.Intn(30000)
-								sleepDuration = time.Duration(numMilli) * time.Millisecond
-							}
-							time.Sleep(sleepDuration)
+					for j := 0; j < numTrials; j++ {
+						sleepDuration := 30 * time.Second
+						if j == 0 {
+							numMilli := rand.Intn(30000)
+							sleepDuration = time.Duration(numMilli) * time.Millisecond
+						}
+						time.Sleep(sleepDuration)
 
+						b.Time("rep bulk loop", func() {
 							b.Time(fmt.Sprintf("%s: fetch actualLRPs", cellID), func() {
 								defer GinkgoRecover()
 								actuals, err := bbsClient.ActualLRPGroups(models.ActualLRPFilter{CellID: cellID})
@@ -50,17 +52,43 @@ var BenchmarkRepFetching = func(numReps, numTrials int) {
 								Expect(ok).To(BeTrue())
 
 								Expect(len(actuals)).To(BeNumerically("~", expectedActualLRPCount, expectedActualLRPVariation))
+
+								numActuals := len(actuals)
+								for k := 0; k < numActuals; k++ {
+									randomNum := rand.Intn(100)
+									isStarting := randomNum < percentWrites
+
+									actualLRP, _ := actuals[k].Resolve()
+
+									b.Time(fmt.Sprintf("%s: claim actual LRP", cellID), func() {
+										index := int(actualLRP.ActualLRPKey.Index)
+										err = bbsClient.ClaimActualLRP(actualLRP.ActualLRPKey.ProcessGuid, index, &actualLRP.ActualLRPInstanceKey)
+										Expect(err).NotTo(HaveOccurred())
+									}, reporter.ReporterInfo{
+										MetricName: RepClaimActualLRP,
+									})
+
+									if isStarting {
+										b.Time(fmt.Sprintf("%s: start actual LRP", cellID), func() {
+											netInfo := models.NewActualLRPNetInfo("1.2.3.4", models.NewPortMapping(61999, 8080))
+											err = bbsClient.StartActualLRP(&actualLRP.ActualLRPKey, &actualLRP.ActualLRPInstanceKey, &netInfo)
+											Expect(err).NotTo(HaveOccurred())
+										}, reporter.ReporterInfo{
+											MetricName: RepStartActualLRP,
+										})
+									}
+								}
 							}, reporter.ReporterInfo{
 								MetricName: RepBulkFetching,
 							})
-						}
-					}(cellID)
-				}
+						}, reporter.ReporterInfo{
+							MetricName: RepBulkLoop,
+						})
+					}
+				}(cellID)
+			}
 
-				wg.Wait()
-			}, reporter.ReporterInfo{
-				MetricName: RepBulkLoop,
-			})
+			wg.Wait()
 		}, 1)
 	})
 }
