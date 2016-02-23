7da71bc85935ff79ae57de814c088e16222b0f3a
Luan Santos <lsantos@pivotal.io>
2016-02-22 16:26:09 -0800

    Use averaging metrics instead of one for each cell
    [#113854839]
    
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>

--- rep_bulk_fetch_test.go
+++ rep_bulk_fetch_test.go
@@ -40,47 +40,43 @@ var BenchmarkRepFetching = func(numReps, numTrials int) {
 						time.Sleep(sleepDuration)
 
 						b.Time("rep bulk loop", func() {
-							b.Time(fmt.Sprintf("%s: fetch actualLRPs", cellID), func() {
-								defer GinkgoRecover()
-								actuals, err := bbsClient.ActualLRPGroups(models.ActualLRPFilter{CellID: cellID})
-								Expect(err).NotTo(HaveOccurred())
+							defer GinkgoRecover()
+							actuals, err := bbsClient.ActualLRPGroups(models.ActualLRPFilter{CellID: cellID})
+							Expect(err).NotTo(HaveOccurred())
 
-								expectedActualLRPCount, ok := expectedActualLRPCounts[cellID]
-								Expect(ok).To(BeTrue())
+							expectedActualLRPCount, ok := expectedActualLRPCounts[cellID]
+							Expect(ok).To(BeTrue())
 
-								expectedActualLRPVariation, ok := expectedActualLRPVariations[cellID]
-								Expect(ok).To(BeTrue())
+							expectedActualLRPVariation, ok := expectedActualLRPVariations[cellID]
+							Expect(ok).To(BeTrue())
 
-								Expect(len(actuals)).To(BeNumerically("~", expectedActualLRPCount, expectedActualLRPVariation))
+							Expect(len(actuals)).To(BeNumerically("~", expectedActualLRPCount, expectedActualLRPVariation))
 
-								numActuals := len(actuals)
-								for k := 0; k < numActuals; k++ {
-									randomNum := rand.Intn(100)
-									isStarting := randomNum < percentWrites
+							numActuals := len(actuals)
+							for k := 0; k < numActuals; k++ {
+								randomNum := rand.Intn(100)
+								isStarting := randomNum < percentWrites
 
-									actualLRP, _ := actuals[k].Resolve()
+								actualLRP, _ := actuals[k].Resolve()
 
-									b.Time(fmt.Sprintf("%s: claim actual LRP", cellID), func() {
-										index := int(actualLRP.ActualLRPKey.Index)
-										err = bbsClient.ClaimActualLRP(actualLRP.ActualLRPKey.ProcessGuid, index, &actualLRP.ActualLRPInstanceKey)
+								b.Time(fmt.Sprintf("claim actual LRP"), func() {
+									index := int(actualLRP.ActualLRPKey.Index)
+									err = bbsClient.ClaimActualLRP(actualLRP.ActualLRPKey.ProcessGuid, index, &actualLRP.ActualLRPInstanceKey)
+									Expect(err).NotTo(HaveOccurred())
+								}, reporter.ReporterInfo{
+									MetricName: RepClaimActualLRP,
+								})
+
+								if isStarting {
+									b.Time(fmt.Sprintf("start actual LRP"), func() {
+										netInfo := models.NewActualLRPNetInfo("1.2.3.4", models.NewPortMapping(61999, 8080))
+										err = bbsClient.StartActualLRP(&actualLRP.ActualLRPKey, &actualLRP.ActualLRPInstanceKey, &netInfo)
 										Expect(err).NotTo(HaveOccurred())
 									}, reporter.ReporterInfo{
-										MetricName: RepClaimActualLRP,
+										MetricName: RepStartActualLRP,
 									})
-
-									if isStarting {
-										b.Time(fmt.Sprintf("%s: start actual LRP", cellID), func() {
-											netInfo := models.NewActualLRPNetInfo("1.2.3.4", models.NewPortMapping(61999, 8080))
-											err = bbsClient.StartActualLRP(&actualLRP.ActualLRPKey, &actualLRP.ActualLRPInstanceKey, &netInfo)
-											Expect(err).NotTo(HaveOccurred())
-										}, reporter.ReporterInfo{
-											MetricName: RepStartActualLRP,
-										})
-									}
 								}
-							}, reporter.ReporterInfo{
-								MetricName: RepBulkFetching,
-							})
+							}
 						}, reporter.ReporterInfo{
 							MetricName: RepBulkLoop,
 						})
